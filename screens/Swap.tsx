3ad61736a2d7a08a3caa244b71cf674054156d09
iris <25711391+Iris314@users.noreply.github.com>
2022-09-12 17:56:23 +0100

    add basic conditional styling to single swap page

--- screens/Swap.tsx
+++ screens/Swap.tsx
@@ -1,17 +1,53 @@
 import React, { useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Card } from "@rneui/themed";
+import { Card, Button } from "@rneui/themed";
+import { UserContext } from "../context/UserContext";
 
 const Swap = ({ route }: any) => {
+  const { currentUser } = useContext(UserContext);
   const swap = route.params.swap;
   {
     return (
       <View style={styles.container}>
-        <Card>
-          <Card.Image
-            style={styles.coverImage}
-            source={{ uri: swap.book1_cover }}></Card.Image>
-        </Card>
+        <View style={styles.row}>
+          <Card>
+            <Card.Image
+              style={styles.coverImage}
+              source={{ uri: swap.book1_cover }}></Card.Image>
+          </Card>
+          <Text style={{ fontSize: 50 }}>&#8644;</Text>
+          <Card>
+            <Card.Image
+              style={styles.coverImage}
+              source={{ uri: swap.book2_cover }}></Card.Image>
+          </Card>
+        </View>
+        {currentUser ? (
+          <View>
+            <View style={styles.row}>
+              <Text>Trading my {swap.book1_title}</Text>
+              <Text>
+                For {swap.user_id2}'s {swap.book2_title}
+              </Text>
+            </View>
+            <View style={styles.buttons}>
+              <Button containerStyle={{ margin: 10 }} title="Accept"></Button>
+              <Button containerStyle={{ margin: 10 }} title="Decline"></Button>
+            </View>
+          </View>
+        ) : (
+          <View>
+            <View style={styles.row}>
+              <Text>
+                Trading {swap.user_id2}'s {swap.book1_title}
+              </Text>
+              <Text>For my {swap.book2_title}</Text>
+            </View>
+            <View style={styles.buttons}>
+              <Button containerStyle={{ margin: 10 }} title="Cancel"></Button>
+            </View>
+          </View>
+        )}
       </View>
     );
   }
@@ -24,9 +60,21 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  buttons: {
+    margin: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  row: {
+    textAlign: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   coverImage: {
-    height: 250,
-    width: 150,
+    height: 160,
+    width: 100,
   },
 });
 
